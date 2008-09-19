7c603b36777d6f17a8be359eaefaf28c09115a7b
Bogdan Opanchuk <bg@bk.ru>
2008-09-20 02:21:48 +0400

    shared: comments translated to Endlish

--- shared/nkcodec.h
+++ shared/nkcodec.h
@@ -1,26 +1,24 @@
-// -----------------------------------------------
-// Заголовочный файл для использования nkcodec.lib
-// -----------------------------------------------
+// ------------------
+// nkcodec.lib header
+// ------------------
 
 #ifndef _NKCODEC_H
 #define _NKCODEC_H
 
-// Создание куска, соответствующего базовому вектору с номером vector_num 
-// src - массив размера length*k, res - массив размера length
+// Create slice, corresponding to basis vector with given number
+// src has size (length*k), res has size (length)
 VOID GFCreateSlice(OUT UCHAR *res, IN UCHAR *src, IN ULONG length, IN UCHAR k, IN UCHAR vector_num);
 
-// Построение матрицы размером k*k для склейки файлов 
-// parts_numbers - номера частей файлов, склейка которых будет производиться
-// temp - область памяти для промежуточных вычислений
-// По указателям res и temp должны быть выделены области памяти размером k*k
+// Create assembling matrix with size k*k
+// parts_numbers - array of slice numbers
+// temp, res - k*k buffers
 VOID GFCreateAssemblingMatrix(OUT UCHAR *res, OUT UCHAR *temp, IN const UCHAR *parts_numbers, IN UCHAR k);
 
-// Функция для декодирования
-// src - массив размера length*n, где по порядку записаны куски длиной length с номерами 1..n
-// vector_nums - массив размера k с номерами кусков, по которым производится декодирование
-// assemble_matrix - матрица декодирования, построенная с помощью GFCreateAssemblingMatrix,
-// причем, в качестве одного из параметров ей должен быть передан тот же массив parts_numbers
-// !!! decode_length (размер декодируемого блока) должен быть кратен k !!
+// Decoding function
+// src has size (length*n), and contains n consecutive slices
+// parts_numbers - has size (k) and contains numbers of slices
+// assemble_matrix - created with the same parts_numbers
+// !!! part_length must be multiple of k !!
 VOID GFAssembleSlices(	OUT UCHAR *res, IN UCHAR *src, ULONG part_length, UCHAR k, 
 						IN UCHAR *parts_numbers, IN UCHAR *assemble_matrix );
 
