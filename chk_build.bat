919a38ae94e50f3437d6d2cda8b5847308e15f77
Bogdan Opanchuk <bg@bk.ru>
2008-09-30 01:22:15 +0400

    Fixed build script to conform to new ddkbuild.cmd

--- chk_build.bat
+++ chk_build.bat
@@ -1,14 +1,16 @@
 echo off
 
+set BASEDIR=c:\DDK
+
 echo *** Building nkcodec ***
-call ddkbuild -quiet -WNET free nkcodec
+call ddkbuild WLHNet free nkcodec
 copy /Y nkcodec\objfre_wnet_x86\i386\nkcodec.lib shared
 
 echo *** Building nkcontrol ***
-call ddkbuild -quiet -WNET free nkcontrol
+call ddkbuild WLHNet free nkcontrol
 
 echo *** Building nkfilter ***
-call ddkbuild -quiet -WNET checked nkfilter
+call ddkbuild WLHNet checked nkfilter
 
 echo *** Copying files ***
 xcopy /Y nkfilter\objchk_wnet_x86\i386\nkfilter.sys compiled\*
