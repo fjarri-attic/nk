a8c0b847fa526378b94ece039ee21a2d93cc2dd8
Bogdan Opanchuk <bg@bk.ru>
2008-10-12 23:47:13 +0400

    psexec moved to \third

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,6 @@
 set VM="c:\data\vm\Win2k3\Windows Server 2003 Enterprise Edition.vmx"
-set BINDIR=c:\nk\compiled
+set REPODIR=c:\nk
+set BINDIR=%REPODIR%\compiled
 set DEPDIR=c:\nk
 
 vmrun revertToSnapshot %VM% ForDeploy
@@ -10,7 +11,7 @@ vmrun -gu Administrator -gp 1q2w3e runProgramInGuest %VM% "%windir%\system32\cmd
 vmrun -gu Administrator -gp 1q2w3e copyFileFromHostToGuest %VM% "%BINDIR%\nkfilter.sys" "%DEPDIR%\nkfilter.sys"
 vmrun -gu Administrator -gp 1q2w3e copyFileFromHostToGuest %VM% "%BINDIR%\nkfilter.inf" "%DEPDIR%\nkfilter.inf"
 vmrun -gu Administrator -gp 1q2w3e copyFileFromHostToGuest %VM% "%BINDIR%\nkcontrol.exe" "%DEPDIR%\nkcontrol.exe"
-vmrun -gu Administrator -gp 1q2w3e copyFileFromHostToGuest %VM% "%BINDIR%\psexec.exe" "%DEPDIR%\psexec.exe"
+vmrun -gu Administrator -gp 1q2w3e copyFileFromHostToGuest %VM% "%REPODIR%\third\psexec.exe" "%DEPDIR%\psexec.exe"
 
 vmrun -gu Administrator -gp 1q2w3e runProgramInGuest %VM% "%DEPDIR%\psexec.exe" -accepteula -i -w "%DEPDIR%" "%DEPDIR%\nkcontrol.exe" install
 vmrun reset %VM% soft
