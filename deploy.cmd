518c1efd18ed21a162738827ff3f1e2e1a3a85b8
Bogdan Opanchuk <bg@bk.ru>
2008-10-13 00:50:14 +0400

    Deploy script do not waits for VM reboot now

--- deploy.cmd
+++ deploy.cmd
@@ -3,15 +3,21 @@ set REPODIR=c:\nk
 set BINDIR=%REPODIR%\compiled
 set DEPDIR=c:\nk
 
+:: Load test snapshot
 vmrun revertToSnapshot %VM% ForDeploy
 vmrun start %VM%
 
+:: Create deployment folder
 vmrun -gu Administrator -gp 1q2w3e runProgramInGuest %VM% "%windir%\system32\cmd.exe" "/c mkdir \"%DEPDIR%\""
 
+:: Copy files to VM
 vmrun -gu Administrator -gp 1q2w3e copyFileFromHostToGuest %VM% "%BINDIR%\nkfilter.sys" "%DEPDIR%\nkfilter.sys"
 vmrun -gu Administrator -gp 1q2w3e copyFileFromHostToGuest %VM% "%BINDIR%\nkfilter.inf" "%DEPDIR%\nkfilter.inf"
 vmrun -gu Administrator -gp 1q2w3e copyFileFromHostToGuest %VM% "%BINDIR%\nkcontrol.exe" "%DEPDIR%\nkcontrol.exe"
 vmrun -gu Administrator -gp 1q2w3e copyFileFromHostToGuest %VM% "%REPODIR%\third\psexec.exe" "%DEPDIR%\psexec.exe"
 
+:: Install nkfilter
 vmrun -gu Administrator -gp 1q2w3e runProgramInGuest %VM% "%DEPDIR%\psexec.exe" -accepteula -i -w "%DEPDIR%" "%DEPDIR%\nkcontrol.exe" install
-vmrun reset %VM% soft
+
+:: Reboot VM
+vmrun -gu Administrator -gp 1q2w3e runProgramInGuest %VM% "%DEPDIR%\psexec.exe" -accepteula shutdown /r /t 0
