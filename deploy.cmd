55af6bb25b441f3ba5f64445c83f662a59bf3ee8
Bogdan Opanchuk <bg@bk.ru>
2009-03-24 00:07:42 +0300

    deploy.cmd fixed according to new repo location

--- deploy.cmd
+++ deploy.cmd
@@ -1,14 +1,14 @@
-set VM="d:\vm\Win2k3\Windows Server 2003 Enterprise Edition.vmx"
-set REPODIR=d:\gitrepos\nk
+set VM="c:\vm\Win2k3\Windows Server 2003 Enterprise Edition.vmx"
+set REPODIR=c:\gitrepos\nk
 set BINDIR=%REPODIR%\compiled
 set DEPDIR=c:\nk
 
 :: Load test snapshot
-vmrun revertToSnapshot %VM% ForDeployVirtualDisk
+vmrun revertToSnapshot %VM% ForDeploy
 vmrun start %VM%
 
 :: Create deployment folder
-vmrun -gu Administrator -gp 1q2w3e runProgramInGuest %VM% "%windir%\system32\cmd.exe" "/c mkdir \"%DEPDIR%\""
+vmrun -gu Administrator -gp 1q2w3e runProgramInGuest %VM% "c:\windows\system32\cmd.exe" "/c mkdir \"%DEPDIR%\""
 
 :: Copy files to VM
 vmrun -gu Administrator -gp 1q2w3e copyFileFromHostToGuest %VM% "%BINDIR%\nkfilter.sys" "%DEPDIR%\nkfilter.sys"
